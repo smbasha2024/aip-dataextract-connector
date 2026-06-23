0a6010e0e238ec9a2de6c1deed1e1a1694c39951
S M Basha <smbasha2024@gmail.com>
2026-06-24 02:51:37 +0530

    socket code change

--- app/websocket/websocket_client.py
+++ app/websocket/websocket_client.py
@@ -6,6 +6,12 @@ from app.database.repository import create_task
 from app.runtime.task_queue import (TASK_QUEUE)
 from app.config.settings import settings
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+RECONNECT_DELAY = 10
+
 async def websocket_client():
     async with websockets.connect(
         settings.server_ws,
@@ -16,14 +22,20 @@ async def websocket_client():
         print("Connected")
 
         while True:
-            message = await websocket.recv()
-            task = json.loads(message)
+            try:
+                message = await websocket.recv()
+                task = json.loads(message)
 
-            task_id = create_task(task)
+                task_id = create_task(task)
 
-            await TASK_QUEUE.put(task_id)
+                await TASK_QUEUE.put(task_id)
 
-            print(
-                f"Task queued "
-                f"{task['job_id']}"
-            )
+                print(
+                    f"Task queued "
+                    f"{task['job_id']}"
+                )
+            except Exception as e:
+                print(f"WebSocket connection failed: {e}")
+                print("Retrying in 10 seconds...")
+
+                await asyncio.sleep(RECONNECT_DELAY)
